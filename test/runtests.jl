1122b919d5af0d4662abdf197046c39ecc94e8b1
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 21:41:09 +0000

    Snapshot of branch master taken on 2019-04-07T17:41:04.589-04:00 from "https://github.com/UnofficialJuliaMirror/GaussianProcesses.jl-891a1506-143c-57d2-908e-e1f8e92e6de9"

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+module TestGaussianProcesses
+@time include("utils.jl")
+@time include("gp.jl")
+@time include("means.jl")
+@time include("kernels.jl")
+@time include("optim.jl")
+@time include("mcmc.jl")
+@time include("gpmc.jl")
+@time include("elastic.jl")
+@time include("memory.jl")
+@time include("test_crossvalidation.jl")
+end
